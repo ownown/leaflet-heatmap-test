e1a0c788a8a502411644d958f927c021fbc41c79
Oliver Newman <oliver@olivernewman.co.uk>
2022-03-16 15:43:06 +0000

    tidy up

--- map.js
+++ map.js
@@ -3,13 +3,12 @@ var map = L.map('map', { crs: L.CRS.Simple });
 var bounds = [[0,0],[1000,1000]];
 var image = L.imageOverlay('fp.png', bounds).addTo(map);
 
+var hmRadius = 25;
+var hmBlur = 20;
+
 map.fitBounds(bounds);
 
-var heat = L.heatLayer([
-    // [100,100,10000],
-    // [120,120,3000],
-    // [400,400,3000]
-], {radius: 25, blur: 20}).addTo(map);
+var heat = L.heatLayer([], {radius: hmRadius, blur: hmBlur}).addTo(map);
 
 function createPoint(map, heatmap, latlng, value)
 {
@@ -22,3 +21,6 @@ function createPoint(map, heatmap, latlng, value)
 }
 
 createPoint(map, heat, [800,800], 6000);
+createPoint(map, heat, [100,100], 10000);
+createPoint(map, heat, [120,120], 3000);
+createPoint(map, heat, [400,400], 3000);
